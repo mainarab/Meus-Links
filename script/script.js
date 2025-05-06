0bb9fb34a76a48162edb13b6c9e3fa87b6b17379
Mainara Barbosa <mainarabarbosadasilva@gmail.com>
2025-05-06 16:20:59 -0300

    Tentativa 1

---
+++ script/script.js
@@ -0,0 +1,12 @@
+
+
+
+
+/* Botao.addEventListener('click', ()=>{
+    Botao.classList.toggle('dark')
+    body.classList.toggle('dark')
+    panda.setAttribute("src", "Images/HappyPanda.svg")
+ 
+}) */
+
+ 
